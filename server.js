a18598d2f52f742defed569a01b3467ce5c82de5
Morormo <morormo@gmail.com>
2020-05-11 14:58:41 +0200

    ADD file validation

--- server.js
+++ server.js
@@ -3,14 +3,31 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
+
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, file } = req.body;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, file: file });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
+
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
